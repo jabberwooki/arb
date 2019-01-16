b3c3b54be91b11b063150f141666436ee873729e
Christophe Espiau <christophe.espiau@gmail.com>
2019-01-16 15:31:31 +0100

    Activation du Diaporama de la page d'accueil dans arb.js.

--- web/themes/custom/arb/js/arb.js
+++ web/themes/custom/arb/js/arb.js
@@ -1,47 +1,52 @@
 (function ($) {
   Drupal.behaviors.news = {
     attach: function (context, settings) {
-        
+      // Diaporama
+      $('#block-views-block-slides-frontpage-slideshow .view-content').slick({
+        dots: true,
+        speed: 1500,
+        autoplay: true,
+        arrows: false,
+        fade: true
+      });
 
-    // Slick carousel les essentiels
-        $('#block-views-block-news-block .view-content').slick({
-          arrows: true,
-          infinite: true,
-          slidesToShow: 4,
-          slidesToScroll: 4,
-              responsive: [
-                {
-                  breakpoint: 1024,
-                  settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                    infinite: true,
-                    arrows: true
-                  }
-                },
-                {
-                  breakpoint: 600,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                  }
-                },
-                {
-                  breakpoint: 480,
-                  settings: {
-                    dots:true,
-                    arrows: false,
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                  }
-                }
-                // You can unslick at a given breakpoint now by adding:
-                // settings: "unslick"
-                // instead of a settings object
-              ]
-        });
-        
-      
+      // Slick carousel les essentiels (Actus)
+      $('#block-views-block-news-block .view-content').slick({
+        arrows: true,
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+          responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                arrows: true
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+              }
+            },
+            {
+              breakpoint: 480,
+              settings: {
+                dots:true,
+                arrows: false,
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
+          ]
+      });
     }
   };
 })(jQuery);
